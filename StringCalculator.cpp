cc04675dfef3f8ba183ab35d7dd37c0a8161bf04
medha7789 <49429968+medha7789@users.noreply.github.com>
2024-06-27 16:46:56 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,1 +1,18 @@
 #include "StringCalculator.h"
+void StringCalculator::tokenize(const std::string& str, const std::string& delimiter, std::vector<int>& tokens) 
+{
+        size_t start = 0, end = 0;
+        while ((end = str.find(delimiter, start)) != std::string::npos) 
+        {
+            std::string token = str.substr(start, end - start);
+            tokens.push_back(std::stoi(token));
+            start = end + delimiter.length();
+        }
+        std::string last_token = str.substr(start);
+        tokens.push_back(std::stoi(last_token));
+}
+int StringCalculator::add(const std::string& numbers)
+{
+  if (numbers.empty()) 
+    return 0;
+}
