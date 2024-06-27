804c2e52cdb55558995b9c81b4f8df57dc5d7786
medha7789 <49429968+medha7789@users.noreply.github.com>
2024-06-27 15:37:14 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,11 +1,11 @@
 #include "StringCalculator.h"
 int StringCalculator::add(string input){
-        if(input.empty())
+        /*if(input.empty())
         {
                 cout<<"entered empty case"<<endl;
                 return 0;
         }
-        else if(input.compare("0"))
+        else*/ if(input.compare("0"))
         {
                 cout<<"entered zero case"<<endl;
                 return 1;
