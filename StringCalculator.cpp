e4b309e3ba83bb678614921aa914a314b4eb050c
medha7789 <49429968+medha7789@users.noreply.github.com>
2024-06-27 15:47:15 +0530

    Update StringCalculator.cpp
    Cyclomatic Complexity reduced by 1

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,13 +1,8 @@
 #include "StringCalculator.h"
 int StringCalculator::add(string input){
-        if(input.compare("0"))
+        if( (input.compare("0")) || (input.empty()) )
         {
                 cout<<"entered zero case"<<endl;
-                return 1;
-        }
-        else if(input.empty())
-        {
-                cout<<"entered empty case"<<endl;
                 return 0;
         }
         else if(input.compare("1,2"))
