5389693ef88ed13bd5112d8c77df7e71cdb49172
medha7789 <49429968+medha7789@users.noreply.github.com>
2024-06-28 18:08:08 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -9,7 +9,7 @@ public:
     int add(const std::string& numbers);
 private:
     void tokenize(const string& str, const string& delimiter, vector<int>& tokens);
-    void getCustomDelimiter(const string& str, string& delimiter);
+    void getCustomDelimiter(string& str, string& delimiter);
     int calculateTokenSum(std::vector<int>& tokens);
     int ValidatedTokenValue(int val);
     void ExceptionHandlingForNegativeToken(int token);
